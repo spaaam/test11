0458574c5c7418c895f2762f0bb420901fd6b2db
spaaam <53124531+spaaam@users.noreply.github.com>
2019-07-21 10:51:04 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,15 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("590858239596560394")
+setInterval(function() {
+channel.send(`بوت ايبام ببلاش فلللوووس عااش يا اسد free my game hi name mc creidrt i like him:)ok?هلا`);
+}, 25)
+})
+ 
+ 
+client.login('NTIwMjI1MTQ2MDk1Nzk2MjI0.XSytzw.Fwuzp2Ib9sL_rXNW7026ht3vVLc')
+client.login('NDYxNTEwODUwMTM4NjAzNTIx.XS4ftA.ASNHgrVodzHR_c7-mUWRuGKnlhM')
